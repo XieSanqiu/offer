ebe68540df4ec332ebaf06c577bb2e70758c2a8d
XieSanqiu <xll_sanqiu@163.com>
2020-08-27 14:48:44 +0800

    feat:新增算法实现

--- niuke/src/sword/Day2.java
+++ niuke/src/sword/Day2.java
@@ -1,6 +1,8 @@
 package sword;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Day2 {
     /**
@@ -57,4 +59,77 @@ public class Day2 {
         }
         return true;
     }
+
+    /**
+     * 使用大顶堆，PriorityQueue默认是小顶堆，但是这个效率好像不行
+     * @param num
+     * @param size
+     * @return
+     */
+    public ArrayList<Integer> maxInWindows(int [] num, int size) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if(num == null || num.length < size || size <= 0){
+            return res;
+        }
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> o2-o1);
+        for(int i = 0; i < size - 1; i++){
+            priorityQueue.add(num[i]);
+        }
+        for(int i = size - 1; i < num.length; i++){
+            priorityQueue.add(num[i]);
+            res.add(priorityQueue.peek());
+            priorityQueue.remove(num[i-size+1]);
+        }
+        return res;
+    }
+
+    /**
+     * 左旋转字符串
+     * @param str
+     * @param n
+     * @return
+     */
+    public String LeftRotateString(String str,int n) {
+        if(str == null || str.length() <= 1){
+            return str;
+        }
+        int rotate = n % str.length();
+        return str.substring(rotate, str.length()) + str.substring(0, rotate);
+    }
+
+    /**
+     * 反转句子
+     * 想考察的主要是先翻转单词，再翻转整个句子
+     * @param str
+     * @return
+     */
+    public String ReverseSentence(String str) {
+        char[] chars = str.toCharArray();
+        int ws = 0, we = 0;
+        while (we <= chars.length){
+            if(we == chars.length || chars[we] == ' '){
+                reverse(chars, ws, we-1);
+                ws = we + 1;
+            }
+            we++;
+        }
+        reverse(chars, 0, chars.length-1);
+        return new String(chars);
+    }
+    private void reverse(char[] chars, int i, int j){
+        while (i < j){
+            swap(chars, i--, j++);
+        }
+    }
+    private void swap(char[] chars, int i, int j){
+        char tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        Day2 day2 = new Day2();
+        System.out.println(day2.maxInWindows(new int[]{2, 3, 4, 2, 6, 2, 5, 1}, 3));
+    }
+
 }
